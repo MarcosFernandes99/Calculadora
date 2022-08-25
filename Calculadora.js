8081975b4e0fb16d33a0163a75384524e27388e3
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 20:47:28 -0300

    Atualizando algoritimo com as funções de subtrair e multiplicar

--- Calculadora.js
+++ Calculadora.js
@@ -3,9 +3,21 @@ var numeroA = parseInt(prompt(`Inisra um numero aqui`))
 var numeroB = parseInt(prompt(`Insira um numero aqui`))
 
 CalcularSoma();
+CalcularSubtracao();
+CalcularMultiplicacao();
 
 function CalcularSoma(){
     console.log(`A soma será calculada aqui!`)
     console.log(numeroA + numeroB)
-}
+    }
+
+function CalcularSubtracao(){
+    console.log(`A subtração será calculada aqui`)
+    console.log(numeroA - numeroB)
+   }
+
+   function CalcularMultiplicacao(){
+    console.log(`A multiplicação será calculada aqui`)
+    console.log(numeroA * numeroB)
+   }
 
