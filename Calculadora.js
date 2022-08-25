79838a9ec1a7c32e958d234ea00ae00dd04ab6bf
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 20:41:27 -0300

    Criando projeto calculadora

---
+++ Calculadora.js
@@ -0,0 +1,11 @@
+
+var numeroA = parseInt(prompt(`Inisra um numero aqui`))
+var numeroB = parseInt(prompt(`Insira um numero aqui`))
+
+CalcularSoma();
+
+function CalcularSoma(){
+    console.log(`A soma será calculada aqui!`)
+    console.log(numeroA + numeroB)
+}
+
