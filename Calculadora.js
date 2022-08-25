917228cd28f960a71d1b8869d429c742b8810d69
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 20:50:32 -0300

    Atualizando algoritmo com a função de divisão

--- Calculadora.js
+++ Calculadora.js
@@ -1,10 +1,12 @@
 
 var numeroA = parseInt(prompt(`Inisra um numero aqui`))
 var numeroB = parseInt(prompt(`Insira um numero aqui`))
+console.log(`Numero A: ${numeroA} - Numero B: ${numeroB}`)
 
 CalcularSoma();
 CalcularSubtracao();
 CalcularMultiplicacao();
+CalcularDivisao();
 
 function CalcularSoma(){
     console.log(`A soma será calculada aqui!`)
@@ -20,4 +22,11 @@ function CalcularSubtracao(){
     console.log(`A multiplicação será calculada aqui`)
     console.log(numeroA * numeroB)
    }
+
+   function CalcularDivisao(){
+    console.log(`A divisão será calculada aqui`)
+    console.log(numeroA / numeroB)
+   }
+
+
 
