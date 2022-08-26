c1fc600fe5b4ea59567fe63308556a4f0c6d0d77
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 21:39:14 -0300

    Atualizando algoritmo com a função de continuar ou encerrar a operação

--- Calculadora.js
+++ Calculadora.js
@@ -4,45 +4,55 @@ var numeroB = parseInt(prompt(`Insira um numero aqui`))
 console.log(`Numero A: ${numeroA} - Numero B: ${numeroB}`)
 var resultadoSoma, resultadoSubtracao, resultadoMultiplicacao, resultadoDivisao
 
+var continuar = true
+while(continuar){
 var opcao = prompt(`Escolha a função/ 1 - Soma/ 2 - Subtração/ 3 - Multiplicação/ 4 - Divisão`)
 
 if(opcao == 1){
     CalcularSoma(numeroA, numeroB);
-    return resultadoSoma
 }    
 
 else if(opcao == 2){
     CalcularSubtracao(numeroA, numeroB);
-    return resultadoSubtracao
 }
     
 else if(opcao == 3){
     CalcularMultiplicacao(numeroA, numeroB);
-    return resultadoMultiplicacao
 }
     
 else{
     CalcularDivisao(numeroA, numeroB);
-    return resultadoDivisao
-}    
+}
 
-    function CalcularSoma(num1, num2){
+var desejacontinuar = prompt(`1 - Fazer outra operação/ 2 - Encerrar`)
+if(desejacontinuar != 1){
+    continuar = false
+}
+
+}
+
+function CalcularSoma(num1, num2){
     console.log(`A soma será calculada aqui!`)
     console.log(numeroA + numeroB)
+    return resultadoSoma
     }
 
     function CalcularSubtracao(num1, num2){
     console.log(`A subtração será calculada aqui`)
     console.log(numeroA - numeroB)
+    return resultadoSubtracao
     }
 
     function CalcularMultiplicacao(num1, num2){
     console.log(`A multiplicação será calculada aqui`)
     console.log(numeroA * numeroB)
+    return resultadoMultiplicacao
     }
 
     function CalcularDivisao(num1, num2){
     console.log(`A divisão será calculada aqui`)
     console.log(numeroA / numeroB)
-    }
+    return resultadoDivisao
+    }   
+
     
