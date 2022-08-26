9eb4dda5fcb6891fe1ae0e66052dc30d7f8c7582
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 21:31:40 -0300

    Corrigindo algoritimo e adicionando comando de escolha

--- Calculadora.js
+++ Calculadora.js
@@ -2,31 +2,47 @@
 var numeroA = parseInt(prompt(`Inisra um numero aqui`))
 var numeroB = parseInt(prompt(`Insira um numero aqui`))
 console.log(`Numero A: ${numeroA} - Numero B: ${numeroB}`)
+var resultadoSoma, resultadoSubtracao, resultadoMultiplicacao, resultadoDivisao
 
-CalcularSoma();
-CalcularSubtracao();
-CalcularMultiplicacao();
-CalcularDivisao();
+var opcao = prompt(`Escolha a função/ 1 - Soma/ 2 - Subtração/ 3 - Multiplicação/ 4 - Divisão`)
 
-function CalcularSoma(){
+if(opcao == 1){
+    CalcularSoma(numeroA, numeroB);
+    return resultadoSoma
+}    
+
+else if(opcao == 2){
+    CalcularSubtracao(numeroA, numeroB);
+    return resultadoSubtracao
+}
+    
+else if(opcao == 3){
+    CalcularMultiplicacao(numeroA, numeroB);
+    return resultadoMultiplicacao
+}
+    
+else{
+    CalcularDivisao(numeroA, numeroB);
+    return resultadoDivisao
+}    
+
+    function CalcularSoma(num1, num2){
     console.log(`A soma será calculada aqui!`)
     console.log(numeroA + numeroB)
     }
 
-function CalcularSubtracao(){
+    function CalcularSubtracao(num1, num2){
     console.log(`A subtração será calculada aqui`)
     console.log(numeroA - numeroB)
-   }
+    }
 
-   function CalcularMultiplicacao(){
+    function CalcularMultiplicacao(num1, num2){
     console.log(`A multiplicação será calculada aqui`)
     console.log(numeroA * numeroB)
-   }
+    }
 
-   function CalcularDivisao(){
+    function CalcularDivisao(num1, num2){
     console.log(`A divisão será calculada aqui`)
     console.log(numeroA / numeroB)
-   }
-
-
-
+    }
+    
